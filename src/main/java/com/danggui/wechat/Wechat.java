2c6bd1d9c5608b74e7aee7e576c24b52d1d885ac
gongxings <1421222481@qq.com>
2018-12-01 18:47:10 +0800

    修改结构

---
+++ src/main/java/com/danggui/wechat/Wechat.java
@@ -0,0 +1,33 @@
+package com.danggui.wechat;
+
+import com.danggui.wechat.controller.LoginController;
+import com.danggui.wechat.core.MsgCenter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.danggui.wechat.face.IMsgHandlerFace;
+
+public class Wechat {
+	private static final Logger LOG = LoggerFactory.getLogger(Wechat.class);
+	private IMsgHandlerFace msgHandler;
+
+	public Wechat(IMsgHandlerFace msgHandler, String qrPath) {
+		System.setProperty("jsse.enableSNIExtension", "false"); // 防止SSL错误
+		this.msgHandler = msgHandler;
+
+		// 登陆
+		LoginController login = new LoginController();
+		login.login(qrPath);
+	}
+
+	public void start() {
+		LOG.info("+++++++++++++++++++开始消息处理+++++++++++++++++++++");
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				MsgCenter.handleMsg(msgHandler);
+			}
+		}).start();
+	}
+
+}
